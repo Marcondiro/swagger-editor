6260776ca3ae50336308473425d7add6b9fb58d3
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2021-07-23 09:45:28 +0200

    remove default vocabulary

--- src/plugins/editor-autosuggest-semantic-annotation/vocabulary.js
+++ src/plugins/editor-autosuggest-semantic-annotation/vocabulary.js
@@ -1,25 +1,3 @@
-let vocabulary = [
-
-  {
-    caption: "city",
-    snippet: "http://dbpedia.org/resource/City",
-  },
-
-  {
-    caption: "pet",
-    snippet: "http://dbpedia.org/resource/Pet",
-  },
-
-  {
-    caption: "id",
-    snippet: "http://dbpedia.org/resource/Identifier",
-  },
-
-  {
-    caption: "tag",
-    snippet: "http://dbpedia.org/resource/Tag_(metadata)",
-  },
-
-];
+let vocabulary = [];
 
 export default vocabulary;
